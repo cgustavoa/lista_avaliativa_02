42f36dbed2ec4c87d4c8fde043050985647b20e4
cgustavoa <142316756+cgustavoa@users.noreply.github.com>
2023-10-31 09:46:48 -0300

    Update q1_lista_02.c

--- q1_lista_02.c
+++ q1_lista_02.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int conjunto[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int novoConjunto[10];
     int tamanho = 10;
-
+    int conjunto[10];
+    int novoConjunto[10];
+    
     for(int i = 0; i < tamanho; i++){
-        printf("%d ", conjunto[i]);
+        scanf("%d", &conjunto[i]);
     }
 
     printf("\n");
@@ -15,21 +15,19 @@ int main() {
         for(int i = 0; i < tamanho; i ++){
             if(i + 1 < tamanho){
                 novoConjunto[i] = conjunto[i] + conjunto[i + 1];
-            }else{
-                novoConjunto[i] = conjunto[i];
             }
         }
 
         for(int i = 0; i < tamanho -1; i++){
-            printf("%d ", novoConjunto[i]);
+            printf("%d", novoConjunto[i]);
             
         }
         printf("\n");
 
-        tamanho = (tamanho - 1);
         for(int i = 0; i < tamanho -1; i++){
             conjunto[i] = novoConjunto[i];
         }
+        tamanho = (tamanho - 1);
     }
     
     return 0;
